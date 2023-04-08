b443deba8f99bb3f7351d5362f5b084295a7b1c1
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-04-09 01:07:40 +0300

    Rename addBook to NewBook and Update

---
+++ src/components/NewBook;.js
@@ -0,0 +1,46 @@
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { addBook } from '../redux/books/booksSlice';
+
+const NewBook = () => {
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  return (
+    <div className="form-wrapper">
+      <h2>Add a new book</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          dispatch(addBook({ title, author }));
+          setAuthor('');
+          setTitle('');
+        }}
+        className="book-form"
+      >
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          name="author"
+          placeholder="Author"
+          required
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <span />
+        <button type="submit"> Add </button>
+      </form>
+    </div>
+  );
+};
+
+export default NewBook;

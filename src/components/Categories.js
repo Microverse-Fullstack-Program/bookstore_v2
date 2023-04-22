236e9f72ed358701c30f90935d3acab29c6f0a95
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-04-23 00:45:56 +0300

    Update Categories component to display filtered books

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,20 +1,61 @@
-import React from 'react';
+import { React } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { checkStatus } from '../redux/categories/categoriesSlice';
+import { checkStatus, filterBooks } from '../redux/categories/categoriesSlice';
+import DisplayBook from './DisplayBook';
 
 const Categories = () => {
-  const dispatch = useDispatch();
+  const { books } = useSelector((state) => state.books);
   const status = useSelector((state) => state.categories.status);
+  const filteredBooks = useSelector((state) => state.categories.filteredBooks);
+  const dispatch = useDispatch();
+
+  const bookGenre = [...new Map(books.map((book) => [book.category, book])).values()];
+
+  const filterHandler = (category) => {
+    dispatch(filterBooks({ books, category }));
+  };
 
   const handleCheckStatus = () => {
     dispatch(checkStatus());
   };
 
   return (
-    <div className="btn-categories">
-      <button type="button" onClick={handleCheckStatus}>Check Status</button>
-      <p>{status}</p>
-    </div>
+    <>
+      <div>
+        {books.length ? (
+          <>
+            <div className="filter-input">
+              <p>Filter Books:</p>
+              <select
+                className="cat-input"
+                onChange={(e) => filterHandler(e.target.value)}
+              >
+                <option value="Unknown">Select Genre</option>
+                {bookGenre.map((genre) => (
+                  <option value={genre.category} key={genre.item_id}>
+                    {' '}
+                    {genre.category}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <ul className="books-wrapper">
+              {filteredBooks && filteredBooks.map((book) => (
+                <li key={book.item_id} className="book">
+                  {' '}
+                  <DisplayBook book={book} />
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : (
+          <div className="btn-categories">
+            <button type="button" onClick={handleCheckStatus}>Check Status</button>
+            <p className="error">{status}</p>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
